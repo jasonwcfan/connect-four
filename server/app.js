a966bf0405a5ec9486539851bd4b00b8e3caa9fd
Jason Fan <jason.wc.fan@gmail.com>
2017-11-03 12:15:43 -0400

    fix to document updating in mongoldb, game works in realtime

--- server/app.js
+++ server/app.js
@@ -28,15 +28,16 @@ const start = async () => {
         // Join a lobby or create a new room, notifiying other room occupants
         var {game, playerId} = await joinRoom();
         client.join(game.gameId);
-        client.broadcast.to(game.gameId).emit('player joined', playerId.toString());
+        client.broadcast.to(game.gameId).emit('player joined', {game});
         client.emit('joined game', {game, playerId});
 
         // On this client attempting to make a new move
         client.on('make move', async (column) => {
             try {
                 // If move is valid, emit new game state to room occupants
-                let nextGameState = await makeMove(playerId, game, column);
-                io.in(game.gameId).emit('new move', {game: nextGameState})
+                let nextGameState = await makeMove(playerId, column);
+                io.in(nextGameState.gameId)
+                    .emit('new move', {game: nextGameState})
             } catch(err) {
                 // If move is invalid, emit error to this client
                 client.emit('invalid move', err.message);
